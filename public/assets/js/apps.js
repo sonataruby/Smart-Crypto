1c85a32d29a54413d574697fd73b40db7773eab6
Roller Davic <rollerdavic@gmail.com>
2021-08-13 19:53:02 +0700

    Update

--- public/assets/js/apps.js
+++ public/assets/js/apps.js
@@ -192,14 +192,14 @@ SmartApps = (function (SmartApps, $, window) {
 
     	var timeConverter = function(UNIX_timestamp){
 		  var a = new Date(UNIX_timestamp * 1000);
-		  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+		  var months = ['1','2','3','4','5','6','7','8','9','10','11','12'];
 		  var year = a.getFullYear();
 		  var month = months[a.getMonth()];
 		  var date = a.getDate();
 		  var hour = a.getHours();
 		  var min = a.getMinutes();
 		  var sec = a.getSeconds();
-		  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+		  var time = date + '/' + month + '/' + year + ' ' + hour + ':' + min + ':' + sec ;
 		  return time;
 		}
     	var tokenInfo = async function(){
