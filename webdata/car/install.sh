6de21a875ac41fb18b21473504c09f093fac60bd
Roller Davic <rollerdavic@gmail.com>
2021-08-16 18:43:24 +0700

    Update

---
+++ webdata/car/install.sh
@@ -0,0 +1,6 @@
+cp layout/*.ejs ./../public/layout/
+cp main.json ./../
+cp components/*.ejs ./../public/components/
+cp -r images/* ./../public/images/
+cp ico/* ./../public/assets/ico/
+cp abi/*.json ./../public/abi/
