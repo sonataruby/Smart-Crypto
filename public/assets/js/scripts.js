6838643164ac04cf4c6e9576facf34f78302fd94
Roller Davic <rollerdavic@gmail.com>
2021-08-18 19:38:10 +0700

    Update

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -39,6 +39,10 @@ function getUrlVars() {
     return vars;
 }
 
+function nl2br(str){
+ return str.replace(/(?:\r\n|\r|\n)/g, '<br>');
+}
+
 var SmartApps = (function (jQ, win, doc){
     "use strict";
     var SmartApps = {AppInfo: {name: "SmartCrypto", package: "1.9.2", version: "1.0.4", author: "SmartCrypto"} };
