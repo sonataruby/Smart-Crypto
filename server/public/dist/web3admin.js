36976b4cde93191145b9d6e500b45c64110a23d4
Roller Davic <rollerdavic@gmail.com>
2021-09-01 21:41:53 +0700

    Update web3admin.js

--- server/public/dist/web3admin.js
+++ server/public/dist/web3admin.js
@@ -458,6 +458,228 @@ SmartApps = (function (SmartApps, $, window) {
     SmartApps.components.docReady.push(SmartApps.tokenSmart.Init);
     return SmartApps;
 })(SmartApps, jQuery, window);
+
+SmartApps = (function (SmartApps, $, window) {
+        "use strict";
+        
+    
+    var contractFarm;
+   
+    var presenterAddress;
+    var investorAddress;
+    var login_wallet;
+    let GAS = 300000; 
+    var blockchain = SmartApps.Blockchain;
+    var ContractAddress = blockchain.address();
+    var token = SmartApps.tokenSmart;
+    
+    SmartApps.tokenFarm = {};
+    
+    SmartApps.tokenFarm.loadContracts = async () => {
+
+                contractFarm = await blockchain.loadContractFarm();
+                
+                login_wallet = await blockchain.getLoginWallet();
+            }
+    SmartApps.tokenFarm.setup = async () => {
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.init();
+                }
+                await token.loadContracts();
+                await token.allowance(ContractAddress.AddressContractFarm);
+            }
+    SmartApps.tokenFarm.allowance = async () => {
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.init();
+                }
+                await token.loadContracts();
+                let amount = await token.allowance(ContractAddress.AddressContractFarm);
+                return amount;
+            }
+    
+    SmartApps.tokenFarm.approve = async (amount) => {
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.init();
+                }
+                const gasPrice = await blockchain.getGasPrice();
+                
+                await token.loadContracts();
+                let depositAmount = blockchain.toWei(amount.toString(),"ether");
+                await token.approve(ContractAddress.AddressContractFarm,depositAmount);
+            }
+    SmartApps.tokenFarm.balance = async () => {
+                let balance = 0;
+                await contractFarm.stakedBalanceOf(login_wallet).call().then((data) => {
+                    console.log(data);
+                });
+            }
+    SmartApps.tokenFarm.getid = async () => {
+                let lastSessionId = 0;
+                await contractFarm.lastSessionIds(ContractAddress.AddressContractSmartToken).call().then((value) => {
+                    lastSessionId = value;
+                });
+                return lastSessionId;
+            }
+            
+   SmartApps.tokenFarm.stakedBalance = async (session_id) => {
+                let balance = 0;
+                await contractFarm.stakedBalance(session_id).call().then((data) => {
+                    balance = data / 10 ** 18;
+                });
+                return balance;
+            }
+    SmartApps.tokenFarm.session = async (session_id) => {
+
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.init();
+                }
+
+                await contractFarm.sessions(session_id).call().then(async (value) => {
+                    console.log(value);
+                });
+            }
+    SmartApps.tokenFarm.earned = async (session_id) => {
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.init();
+                }
+
+                await contractFarm.earned(session_id, login_wallet).call().then(async (value) => {
+                    console.log(value);
+                });
+            }
+    SmartApps.tokenFarm.pool = async (amount, session_id) => {
+                
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.init();
+                }
+                const gasPrice = await blockchain.getGasPrice();
+                
+                await token.loadContracts();
+                let depositAmount = blockchain.toWei(amount.toString(),"ether");
+                let CheckAppreve = await token.approve(ContractAddress.AddressContractFarm,depositAmount);
+                
+                await contractFarm.deposit(session_id, depositAmount).send({from: login_wallet, gasPrice: gasPrice, gas: GAS}).then((value) => {
+                    console.log(value);
+                });
+                
+            }
+    SmartApps.tokenFarm.createpool  = async (amount, session_id) => {
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.init();
+                }
+
+                const gasPrice = await blockchain.getGasPrice();
+                let depositAmount = blockchain.toWei(amount.toString(),"ether");
+
+                let appoveAmount = await token.allowance(ContractAddress.AddressContractFarm);
+               
+                if(appoveAmount >= amount){
+                    $('#FarmDesopit').modal('show');
+                }else{
+                    await token.approve(ContractAddress.AddressContractFarm,depositAmount).then(() => {
+                        $('#FarmDesopit').modal('show');
+                    });
+                    
+                }
+                
+
+    }
+    SmartApps.tokenFarm.withdraw = async (session_id) => {
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.init();
+                }
+
+                const gasPrice = await blockchain.getGasPrice();
+                
+               
+                let balance = 0;
+                await contractFarm.stakedBalanceOf(session_id,login_wallet).call().then( async (data) => {
+                    if(data == 0){
+                        blockchain.notify("You not join this pool");
+                    }else{
+                        let depositAmount = data.toString();
+
+                        await contractFarm.withdraw(session_id, depositAmount).send({from: login_wallet, gasPrice: gasPrice, gas: GAS}).then(async (value) => {    
+                            blockchain.notify("Confirm success<br>Hash : "+value.transactionHash);
+                        });
+                    }
+                });
+                
+                
+                
+    }
+    SmartApps.tokenFarm.confirm = async (amount, session_id) => {
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.connect();
+                }
+                const gasPrice = await blockchain.getGasPrice();
+                let depositAmount = blockchain.toWei(amount.toString(),"ether");
+                await contractFarm.deposit(session_id, depositAmount).send({from: login_wallet, gasPrice: gasPrice, gas: GAS}).then(async (value) => {
+                   
+                    if(value.status == false){
+                        blockchain.notify("Confirm Error");
+                    }else if(value.status == true){
+                        blockchain.notify("Confirm success<br>Hash : "+value.transactionHash);
+                        await axios.get("/farm/task/"+login_wallet+"/join/"+value.transactionHash+"/"+session_id);
+                    }
+                });
+            }
+    SmartApps.tokenFarm.claim = async (lastSessionId) => {
+
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.connect();
+                }
+                
+                const gasPrice = await blockchain.getGasPrice();
+                await contractFarm.claimable(lastSessionId,login_wallet).call().then( async (data) => {
+                    if(data == 0){
+                        blockchain.notify("Balance empty. You can not claim");
+                    }else{
+
+                        await contractFarm.claim(lastSessionId).send({from: login_wallet, gasPrice: gasPrice, gas:GAS}).then(async (value) => {
+                            blockchain.notify("Claim farm success<br>Hash : "+value.transactionHash);
+                            if(window.TelegramChannel != "" && window.TelegramChannel != undefined){
+                                await axios.post('https://api.telegram.org/bot1962248837:AAGecDXTz2hnsdauDN--mOafqBYS5o-jQsg/sendMessage', {
+                                        chat_id: window.TelegramChannel,
+                                        text: `Farm earn : ${value.transactionHash}`,
+                                        parse_mode:'Markdown'
+                                });
+                            }
+                            
+                        });
+                    }
+                });
+            }
+    SmartApps.tokenFarm.claimNft = async (lastSessionId) => {
+
+                let status = await blockchain.isStatus();
+                if(status == false){
+                    await blockchain.connect();
+                }
+                
+                const gasPrice = await blockchain.getGasPrice();
+                await contractFarm.claimNft(lastSessionId).send({from: login_wallet, gasPrice: gasPrice, gas:GAS}).then((value) => {
+                    console.log(value);
+                });
+            }
+            
+    SmartApps.tokenFarm.Init = () => {
+
+    }
+    SmartApps.components.docReady.push(SmartApps.tokenFarm.Init);
+
+ return SmartApps;
+})(SmartApps, jQuery, window);
 SmartApps = function (SmartApps, $, window, document) {
     "use strict";
     var blockchain = SmartApps.Blockchain;
