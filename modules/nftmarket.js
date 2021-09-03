dc63fe4941d963d3b1357a0cbf78392ab60152e3
Roller Davic <rollerdavic@gmail.com>
2021-09-03 12:42:06 +0700

    Update

--- modules/nftmarket.js
+++ modules/nftmarket.js
@@ -38,17 +38,59 @@ module.exports = function(prefix , app) {
 		    	await contract.paramsOf(index).call().then(async (value) => {
 		    		sql = "SELECT * FROM `nft_smart` WHERE tokenId='"+index+"' LIMIT 1";
     				item = await db.dbQuery(sql, true);
-    				console.log(item);
+    				var readObject = {};
     				if(item == undefined){
-    					
+    					readObject = await insert_items(index, value.quality, value.generation);
+
+    				}else{
+    					readObject = JSON.parse(item.data);
+
     				}
-                	object.push({tokenId:index, quality: value.quality, generation : value.generation});
+    				readObject.attributes[0].value = value.quality;
+    				readObject.attributes[1].value = value.generation;
+                	object.push(readObject);
                 });
 		    }
 
 
 		    return object;
     		
+		}
+
+		const insert_items = async (tokenID, quality, generation) =>{
+			const data = {
+		      "attributes": [{
+		              "trait_type": "QUALITY",
+		              "value": quality
+		            },
+		            {
+		              "trait_type": "Generation",
+		              "value": generation
+		            },
+		            {
+		              "display_type": "boost_number",
+		              "trait_type": "Power",
+		              "value": 2000
+		            },
+		            {
+		              "display_type": "boost_number",
+		              "trait_type": "map_point",
+		              "value": 130
+		            }
+		          ],
+		      "description": "No Description",
+		      "external_url": "https://cryptocar.cc/api/nft/"+tokenID,
+		      "image": "https://cryptocar.cc/nfts/"+generation+".gif",
+		      "name": "CFX 17",
+		      "animation_url": "",
+		      "youtube_url": "",
+		      "facebook_url": "",
+		      "tiwter_url": "",
+		      "smart_url": ""
+		    }
+			sql = "INSERT INTO `nft_smart` SET tokenId='"+tokenID+"', data='"+JSON.stringify(data)+"'";
+    		await db.dbQuery(sql, true);
+    		return data;
 		}
 
 		app.get(prefix, (req, res) => {
@@ -65,12 +107,12 @@ module.exports = function(prefix , app) {
 		 res.render(dataMain.public.market == true ? "market" : "coming",dataMain);
 		});
 
-		app.get(prefix + "/wallet/:wallet", (req, res) => {
+		app.get(prefix + "/wallet/:wallet", async (req, res) => {
 		  app.set('layout', config.layout.dir + "/pages");
 		  const dataMain = fsFile.readJSONFile('market.json');
 		  var wallet = req.params.wallet;
-		  getItems(wallet);
-
+		  var data = await getItems(wallet);
+		  dataMain.items = data;
 		 res.render(dataMain.public.market == true ? "market-wallet" : "coming",dataMain);
 		});
 
